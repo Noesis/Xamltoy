d5ca0cdf4f5d372f538bb7bdc41b8348a00708a6
pabloegpf1 <pabloescrivagallardo@gmail.com>
2020-01-03 14:00:09 +0100

    Refactored index.js

--- src/index.js
+++ src/index.js
@@ -6,41 +6,18 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Router />, document.getElementById('root'));
 
-let verticalHandler = document.getElementById('verticalSplitter');
-let horizontalHandler = document.getElementById('dataContextSplitter');
-
 let horizontalDragging, verticalDragging = false;
 
 document.addEventListener('mousedown', function (e) {
-    if (e.target === verticalHandler) {
-        verticalDragging = true;
-    } else if (e.target === horizontalHandler) {
-        if (!document.getElementById('dataContextContainer').classList.contains('collapsed'))
-            horizontalDragging = true;
-    }
+    let dataContextCollapsed = document.getElementById('dataContextSplitter').classList.contains('collapsed');
+    verticalDragging = e.target === document.getElementById('verticalSplitter');
+    horizontalDragging = e.target === document.getElementById('dataContextSplitter') && !dataContextCollapsed;
 });
 
 document.addEventListener('mousemove', function (e) {
     e.preventDefault();
-    if (verticalDragging){
-        document.getElementById('editorBoxLeft').style.width = e.clientX + 'px';
-        let rightWidth = (Math.max(30, window.innerWidth - e.clientX)) ;
-        document.getElementById('editorBoxRight').style.width = rightWidth + 'px';
-        global.dispatchEvent(new Event('resize'));
-    } else if (horizontalDragging){
-        document.getElementById('dataContextContainer').style.flexBasis = ''
-        document.getElementById('xamlEditorContainer').style.height = (e.clientY - 48) + 'px';
-        let bottomHeight = Math.max( 30, window.innerHeight - e.clientY ) + 'px';
-        if (bottomHeight === '30px'){
-            document.getElementById('dataContextContainer').classList.add('collapsed');
-            document.getElementById('dataContextArrow').style.transform = 'rotate(180deg)'
-        }else{
-            document.getElementById('dataContextContainer').classList.remove('collapsed');
-        }
-        document.getElementById('dataContextContainer').style.height = bottomHeight;
-    }else{
-        return false; 
-    }
+    if (verticalDragging) handleVerticalResize(e);    
+    if (horizontalDragging) handleHorizontalResize(e);
 });
 
 document.addEventListener('mouseup', function (e) {
@@ -51,24 +28,50 @@ document.addEventListener('mouseup', function (e) {
 let _privateError = console.error;
 console.error = function () {
     _privateError.apply(console, arguments);
-    var args = Array.prototype.slice.call(arguments);
-    for (let i = 0; i < args.length; i++) {
-        let node = document.createElement("div");
-        if (args[i].includes("[NOESIS/")) {
-            let text = args[i];
-            let lineNumber = text.substring(text.lastIndexOf("(") + 1, text.lastIndexOf(")"));
-            let error = document.createTextNode(text.substring(text.indexOf(">") + 1));
-            let image = document.createElement("img");
-            image.src = "images/cross.png";
-            node.appendChild(image);
-            node.appendChild(error);
-            node.classList.add('error');
-            document.getElementById("errorLog").appendChild(node);
-            let errorLine = document.getElementsByClassName('CodeMirror-line')[lineNumber-1]
-            //errorLine.style.textDecoration = 'underline red';
-            errorLine.style.backgroundColor = 'rgba(192, 48, 48, 0.3)';
+    var logs = Array.prototype.slice.call(arguments);
+    logs.forEach( line => {
+        if (line.includes("[NOESIS/E]")) {
+            let lineNumber = line.substring(line.lastIndexOf("(") + 1, line.lastIndexOf(")"));
+            generateErrorMessage(line);
+            hightlightLine(lineNumber - 1);
         }
+    });
+}
+
+function handleVerticalResize(e) {
+    let rightWidth = Math.max(30, window.innerWidth - e.clientX);
+    document.getElementById('editorBoxLeft').style.width = e.clientX + 'px';
+    document.getElementById('editorBoxRight').style.width = rightWidth + 'px';
+    global.dispatchEvent(new Event('resize'));
+}
+
+function handleHorizontalResize(e) {
+    let bottomHeight = Math.max(62, window.innerHeight - e.clientY) + 'px';
+    document.getElementById('dataContextContainer').style.height = bottomHeight;
+    document.getElementById('xamlEditorContainer').style.height = (e.clientY - 47) + 'px';
+    if (bottomHeight === '62px') {
+        document.getElementById('dataContextContainer').classList.add('collapsed');
+        document.getElementById('dataContextArrow').style.transform = 'rotate(180deg)';
+        horizontalDragging = false;
+    } else {
+        document.getElementById('dataContextContainer').classList.remove('collapsed');
     }
+}
+
+function generateErrorMessage(log) {
+    let node = document.createElement("div");
+    let errorMessage = document.createTextNode(log.substring(log.indexOf(">") + 1));
+    let icon = document.createElement("img");
+    icon.src = "images/cross.png";
+    node.appendChild(icon);
+    node.appendChild(errorMessage);
+    node.classList.add('error');
+    document.getElementById("errorLog").appendChild(node);
+}
+
+function hightlightLine(lineNumber) {
+    let errorLine = document.getElementsByClassName('CodeMirror-line')[lineNumber]
+    errorLine.style.backgroundColor = 'rgba(192, 48, 48, 0.3)';
 }
 
 serviceWorker.unregister();
