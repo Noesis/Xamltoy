259d8c6c98be47e96047313ce2e46de61c22c743
pabloegpf1 <pabloescrivagallardo@gmail.com>
2020-01-29 15:19:10 +0100

    Refactored index.js

--- src/index.js
+++ src/index.js
@@ -9,118 +9,132 @@ ReactDOM.render(<Router />, document.getElementById('root'));
 let horizontalDragging, verticalDragging = false;
 let leftWidth = window.innerWidth / 2;
 let rightWidth = window.innerWidth - leftWidth - 1;
-let editorWidthRatio = leftWidth/window.innerWidth;
+let editorWidthRatio = leftWidth / window.innerWidth;
 
-resizeEditor();
-resetCursor();
+if (document.getElementById('editorSkeleton')) {
 
-window.addEventListener('resize', () => {
-    leftWidth = Math.round(window.innerWidth * editorWidthRatio);
-    rightWidth = window.innerWidth - leftWidth - 1;
-    editorWidthRatio = Math.min(0.8, leftWidth / window.innerWidth);
-    resizeEditor();
-})
-
-document.addEventListener('mousedown', function (e) {
-    let dataContextCollapsed = document.getElementById('dataContextSplitter').classList.contains('collapsed');
-    verticalDragging = e.target === document.getElementById('verticalSplitter');
-    horizontalDragging = e.target === document.getElementById('dataContextSplitter') && !dataContextCollapsed;
-});
-
-document.addEventListener('mousemove', function (e) {
-    e.preventDefault();
-    if (verticalDragging) handleVerticalResize(e);    
-    if (horizontalDragging) handleHorizontalResize(e);
-    resetCursor();
-});
-
-document.addEventListener('mouseup', function (e) {
-    if (horizontalDragging) checkIfDataContextCollapsed();
-    horizontalDragging = false;
-    verticalDragging = false;
-    resetCursor();
-});
-
-document.addEventListener('mouseout', function (e) {
-    resetCursor();
-});
-
-function resetCursor(){
-    document.getElementById('editorSkeleton').style.pointerEvents = 'auto';
-    document.getElementById('root').style.cursor = 'auto';
-}
-
-function handleVerticalResize(e) {
-    leftWidth = Math.round(e.clientX);
-    rightWidth = window.innerWidth - leftWidth - 1;
-    editorWidthRatio = Math.min(0.8,leftWidth / window.innerWidth);
-    window.dispatchEvent(new Event('resize'));
-    resizeEditor();
-}
-
-function handleHorizontalResize(e) {
-    let editorWidthRatio = 1 - (e.clientY - 47) / (window.innerHeight - 80);
-    document.getElementById('editorSkeleton').style.pointerEvents = 'none';
-    document.getElementById('root').style.cursor = 'ns-resize';
-    document.getElementById('xamlEditorContainer').style.height = 100 - editorWidthRatio * 100 + '%';
-    document.getElementById('dataContextContainer').style.height = editorWidthRatio * 100 + '%';
-}
-
-function resizeEditor() {
-    document.getElementById('editorSkeleton').style.pointerEvents = 'none';
-    document.getElementById('root').style.cursor = 'ew-resize';
-    document.getElementById('editorBoxLeft').style.width = leftWidth + 'px';
-    document.getElementById('editorBoxRight').style.width = rightWidth + 'px';
-    resizeCanvas();
-}
-
-function resizeCanvas() {
+    let root = document.getElementById('root');
+    let dataContextSplitter = document.getElementById('dataContextSplitter');
+    let verticalSplitter = document.getElementById('verticalSplitter');
+    let editorSkeleton = document.getElementById('editorSkeleton');
+    let errorLog = document.getElementById("errorLog");
+    let xamlEditorContainer = document.getElementById('xamlEditorContainer');
+    let dataContextContainer = document.getElementById('dataContextContainer');
+    let dataContextArrow = document.getElementById('dataContextArrow');
+    let editorBoxLeft = document.getElementById('editorBoxLeft');
+    let editorBoxRight = document.getElementById('editorBoxRight');
     let canvas = document.getElementById("canvas");
-    canvas.width = document.getElementById('editorBoxRight').clientWidth;
-    canvas.height = document.getElementById('editorBoxRight').clientHeight;
-}
 
-function checkIfDataContextCollapsed(){
-    if (document.getElementById('dataContextContainer').clientHeight <= 30) {
-        document.getElementById('xamlEditorContainer').style.height = '100%';
-        document.getElementById('dataContextContainer').classList.add('collapsed');
-        document.getElementById('dataContextSplitter').classList.remove('canScroll');
-        document.getElementById('dataContextArrow').style.transform = 'rotate(180deg)';
-        horizontalDragging = false;
-    } else {
-        document.getElementById('dataContextContainer').classList.remove('collapsed');
-        document.getElementById('dataContextSplitter').classList.add('canScroll');
-    }
-}
+    resizeEditor();
+    resetCursor();
 
-function generateErrorMessage(log) {
-    let node = document.createElement("div");
-    let errorMessage = document.createTextNode(log.substring(log.indexOf(">") + 1));
-    let icon = document.createElement("img");
-    document.getElementById('root').style.userSelect = 'auto';
-    icon.src = "images/cross.png";
-    node.appendChild(icon);
-    node.appendChild(errorMessage);
-    node.classList.add('error');
-    document.getElementById("errorLog").appendChild(node);
-}
+    window.addEventListener('resize', () => {
+        leftWidth = Math.round(window.innerWidth * editorWidthRatio);
+        rightWidth = window.innerWidth - leftWidth - 1;
+        editorWidthRatio = Math.min(0.8, leftWidth / window.innerWidth);
+        resizeEditor();
+    })
 
-function hightlightLine(lineNumber) {
-    let errorLine = document.getElementsByClassName('CodeMirror-line')[lineNumber]
-    errorLine.style.backgroundColor = 'rgba(192, 48, 48, 0.3)';
-}
-
-let _privateError = console.error;
-console.error = function () {
-    _privateError.apply(console, arguments);
-    var logs = Array.prototype.slice.call(arguments);
-    logs.forEach(line => {
-        if (line.includes("[NOESIS/E]")) {
-            let lineNumber = line.substring(line.lastIndexOf("(") + 1, line.lastIndexOf(")"));
-            generateErrorMessage(line);
-            hightlightLine(lineNumber - 1);
-        }
+    document.addEventListener('mousedown', function (e) {
+        let dataContextCollapsed = dataContextSplitter.classList.contains('collapsed');
+        verticalDragging = e.target === verticalSplitter;
+        horizontalDragging = e.target === dataContextSplitter && !dataContextCollapsed;
     });
+
+    document.addEventListener('mousemove', function (e) {
+        e.preventDefault();
+        if (verticalDragging) handleVerticalResize(e);
+        if (horizontalDragging) handleHorizontalResize(e);
+        resetCursor();
+    });
+
+    document.addEventListener('mouseup', function (e) {
+        if (horizontalDragging) checkIfDataContextCollapsed();
+        horizontalDragging = false;
+        verticalDragging = false;
+        resetCursor();
+    });
+
+    document.addEventListener('mouseout', function (e) {
+        resetCursor();
+    });
+
+    function resetCursor() {
+        editorSkeleton.style.pointerEvents = 'auto';
+        root.style.cursor = 'auto';
+    }
+
+    function handleVerticalResize(e) {
+        leftWidth = Math.round(e.clientX);
+        rightWidth = window.innerWidth - leftWidth - 1;
+        editorWidthRatio = Math.min(0.8, leftWidth / window.innerWidth);
+        window.dispatchEvent(new Event('resize'));
+        resizeEditor();
+    }
+
+    function handleHorizontalResize(e) {
+        let editorWidthRatio = 1 - (e.clientY - 47) / (window.innerHeight - 80);
+        editorSkeleton.style.pointerEvents = 'none';
+        root.style.cursor = 'ns-resize';
+        xamlEditorContainer.style.height = 100 - editorWidthRatio * 100 + '%';
+        dataContextContainer.style.height = editorWidthRatio * 100 + '%';
+    }
+
+    function resizeEditor() {
+        editorSkeleton.style.pointerEvents = 'none';
+        root.style.cursor = 'ew-resize';
+        editorBoxLeft.style.width = leftWidth + 'px';
+        editorBoxRight.style.width = rightWidth + 'px';
+        resizeCanvas();
+    }
+
+    function resizeCanvas() {
+        canvas.width = editorBoxRight.clientWidth;
+        canvas.height = editorBoxRight.clientHeight;
+    }
+
+    function checkIfDataContextCollapsed() {
+        if (dataContextContainer.clientHeight <= 30) {
+            xamlEditorContainer.style.height = '100%';
+            dataContextContainer.classList.add('collapsed');
+            dataContextSplitter.classList.remove('canScroll');
+            dataContextArrow.style.transform = 'rotate(180deg)';
+            horizontalDragging = false;
+        } else {
+            dataContextContainer.classList.remove('collapsed');
+            dataContextSplitter.classList.add('canScroll');
+        }
+    }
+
+    function generateErrorMessage(log) {
+        let node = document.createElement("div");
+        let errorMessage = document.createTextNode(log.substring(log.indexOf(">") + 1));
+        let icon = document.createElement("img");
+        root.style.userSelect = 'auto';
+        icon.src = "images/cross.png";
+        node.appendChild(icon);
+        node.appendChild(errorMessage);
+        node.classList.add('error');
+        errorLog.appendChild(node);
+    }
+
+    function hightlightLine(lineNumber) {
+        let errorLine = document.getElementsByClassName('CodeMirror-line')[lineNumber]
+        errorLine.style.backgroundColor = 'rgba(192, 48, 48, 0.3)';
+    }
+
+    let _privateError = console.error;
+    console.error = function () {
+        _privateError.apply(console, arguments);
+        var logs = Array.prototype.slice.call(arguments);
+        logs.forEach(line => {
+            if (line.includes("[NOESIS/E]")) {
+                let lineNumber = line.substring(line.lastIndexOf("(") + 1, line.lastIndexOf(")"));
+                generateErrorMessage(line);
+                hightlightLine(lineNumber - 1);
+            }
+        });
+    }
 }
 
 serviceWorker.unregister();
