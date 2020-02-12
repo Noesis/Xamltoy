fdf7da75f467110e78d3f99b406184c98e6e2945
s-fernandez-v <sfernandez@noesisengine.com>
2020-02-12 18:58:56 +0100

    - Completed properties of UIElement, FrameworkElement, Control, Shape, Brush - Added all types of Panel - Added all types of Shape - Added all types of Geometry

--- src/components/Editor/codemirror/schema.js
+++ src/components/Editor/codemirror/schema.js
@@ -1,44 +1,74 @@
 let Schema = {
-  "!top": ["Grid", "StackPanel"],
+  "!top": [
+    "Canvas", "StackPanel", "WrapPanel", "DockPanel", "Grid", "UniformGrid",
+    "Control", "ContentControl", "UserControl", "Page", "HeaderedContentControl",
+    "Border"
+  ],
   "!attrs": {
     "x:Name": null,
     "x:Key": null
   },
   ResourceDictionary: {
-    children: ["FrameworkTemplate", "Brush", "Transform", "UIElement"]
+    children: ["FrameworkTemplate", "Brush", "Transform", "Geometry", "UIElement"]
   },
   UIElement: {
     attrs: {
-      IsEnabled: ["True", "False"],
+      AllowDrop: ["True", "False"],
+      Clip: "Geometry",
+      ClipToBounds: ["True", "False"],
       Focusable: ["True", "False"],
+      IsEnabled: ["True", "False"],
+      IsHitTestVisible: ["True", "False"],
+      IsManipulationEnabled: ["True", "False"],
+      OpacityMask: "Brush",
+      Opacity: null,
       RenderTransformOrigin: null,
       RenderTransform: "Transform",
-      LayoutTransform: "Transform",
       Visibility: ["Collapsed", "Hidden", "Visible"]
     }
   },
   FrameworkElement: {
     attrs: {
-      Width: null,
+      Cursor: [
+        "None", "No", "Arrow", "AppStarting", "Cross", "Help", "IBeam", "SizeAll", "SizeNESW", "SizeNS", "SizeNWSE", "SizeWE", "UpArrow", "Wait", "Hand", "Pen",
+        "ScrollNS", "ScrollWE", "ScrollAll", "ScrollN", "ScrollS", "ScrollW", "ScrollE", "ScrollNW", "ScrollNE", "ScrollSW", "ScrollSE", "ArrowCD"
+      ],
+      DataContext: null,
+      DefaultStyleKey: null,
+      FocusVisualStyle: "Style",
+      ForceCursor: ["True", "False"],
       Height: null,
-      MinWidth: null,
+      HorizontalAlignment: ["Left", "Right", "Center", "Stretch"],
+      InputScope: [
+        "Default", "Url", "FullFilePath", "FileName", "EmailUserName", "EmailSmtpAddress", "LogOnName", "PersonalFullName", "PersonalNamePrefix", "PersonalGivenName",
+        "PersonalMiddleName", "PersonalSurname", "PersonalNameSuffix", "PostalAddress", "PostalCode", "AddressStreet", "AddressStateOrProvince", "AddressCity",
+        "AddressCountryName", "AddressCountryShortName", "CurrencyAmountAndSymbol", "CurrencyAmount", "Date", "DateMonth", "DateDay", "DateYear", "DateMonthName",
+        "DateDayName", "Digits", "Number", "OneChar", "Password", "TelephoneNumber", "TelephoneCountryCode", "TelephoneAreaCode", "TelephoneLocalNumber", "Time",
+        "TimeHour", "TimeMinorSec", "NumberFullWidth", "AlphanumericHalfWidth", "AlphanumericFullWidth", "CurrencyChinese", "Bopomofo", "Hiragana", "KatakanaHalfWidth",
+        "KatakanaFullWidth", "Hanja", "PhraseList", "RegularExpression", "Srgs", "Xml"
+      ],
+      LayoutTransform: "Transform",
+      Margin: null,
+      MaxHeight: null,
       MaxWidth: null,
       MinHeight: null,
-      MaxHeight: null,
-      Margin: null,
-      HorizontalAlignment: ["Left", "Right", "Center", "Stretch"],
-      VerticalAlignment: ["Top", "Bottom", "Center", "Stretch"],
-      Resources: "ResourceDictionary",
+      MinWidth: null,
+      Name: null,
+      OverridesDefaultStyle: ["True", "False"],
       Style: "Style",
       Tag: null,
-      DataContext: null,
+      ToolTip: null,
+      UseLayoutRounding: ["True", "False"],
+      VerticalAlignment: ["Top", "Bottom", "Center", "Stretch"],
+      Width: null,
+      Resources: "ResourceDictionary",
       Triggers: "TriggerBase"
     },
     base: "UIElement"
   },
   Decorator: {
     attrs: {
-      Child: null
+      Child: "UIElement"
     },
     base: "FrameworkElement",
     children: ["UIElement"]
@@ -57,35 +87,77 @@ let Schema = {
     type: "abstract",
     attrs: {
       Background: "Brush",
-      Children: null
+      Children: "UIElement"
     },
     base: "FrameworkElement",
     children: ["UIElement"]
+  },
+  Canvas: {
+    attrs: {},
+    base: "Panel"
+  },
+  StackPanel: {
+    attrs: {
+      Orientation: ["Horizontal", "Vertical"]
+    },
+    base: "Panel"
+  },
+  WrapPanel: {
+    attrs: {
+      ItemWidth: null,
+      ItemHeight: null,
+      Orientation: ["Horizontal", "Vertical"]
+    },
+    base: "Panel"
+  },
+  DockPanel: {
+    attrs: {
+      LastChildFill: ["True", "False"]
+    },
+    base: "Panel"
   },
   Grid: {
     attrs: {},
     base: "Panel"
   },
-  StackPanel: {
+  UniformGrid: {
     attrs: {},
     base: "Panel"
   },
   Control: {
     attrs: {
-      Foreground: "Brush",
       Background: "Brush",
       BorderBrush: "Brush",
       BorderThickness: null,
-      Padding: null
+      FontFamily: null,
+      FontSize: null,
+      FontStretch: ["UltraCondensed", "ExtraCondensed", "Condensed", "SemiCondensed", "Normal", "Medium", "SemiExpanded", "Expanded", "ExtraExpanded", "UltraExpanded"],
+      FontStyle: ["Normal", "Oblique", "Italic"],
+      FontWeight: ["Thin", "ExtraLight", "UltraLight", "Light", "SemiLight", "Normal", "Regular", "Medium", "DemiBold", "SemiBold", "Bold", "ExtraBold", "UltraBold", "Black", "Heavy", "ExtraBlack", "UltraBlack"],
+      Foreground: "Brush",
+      HorizontalContentAlignment: ["Left", "Center", "Right", "Stretch"],
+      IsTabStop: ["True", "False"],
+      Padding: null,
+      TabIndex: null,
+      Template: "ControlTemplate",
+      VerticalContentAlignment: ["Top", "Center", "Bottom", "Stretch"]
     },
     base: "FrameworkElement"
   },
   ContentControl: {
     attrs: {
-      Content: null
+      Content: "UIElement"
     },
     base: "Control",
     children: ["UIElement"]
+  },
+  UserControl: {
+    attrs: {},
+    base: "ContentControl",
+  },
+  Page: {
+    attrs: {},
+    base: "UserControl"
   },
   ButtonBase: {
     type: "abstract",
@@ -103,8 +175,16 @@ let Schema = {
   Shape: {
     type: "abstract",
     attrs: {
-      Fill: "LinearGradientBrush",
-      Stroke: null,
+      Fill: "Brush",
+      Stretch: ["None", "Fill", "Uniform", "UniformToFill"],
+      Stroke: "Brush",
+      StrokeDashArray: null,
+      StrokeDashCap: ["Flat", "Square", "Round", "Triangle"],
+      StrokeDashOffset: null,
+      StrokeEndLineCap: ["Flat", "Square", "Round", "Triangle"],
+      StrokeLineJoin: ["Miter", "Bevel", "Round"],
+      StrokeMiterLimit: null,
+      StrokeStartLineCap: ["Flat", "Square", "Round", "Triangle"],
       StrokeThickness: null
     },
     base: "FrameworkElement"
@@ -117,10 +197,27 @@ let Schema = {
     attrs: {},
     base: "Shape"
   },
+  Line: {
+    attrs: {
+      X1: null,
+      X2: null,
+      Y1: null,
+      Y2: null
+    },
+    base: "Shape"
+  },
+  Path: {
+    attrs: {
+      Data: "Geometry"
+    },
+    base: "Shape"
+  },
   Brush: {
     type: "abstract",
     attrs: {
-      Opacity: null
+      Opacity: null,
+      RelativeTransform: "Transform",
+      Transform: "Transform"
     }
   },
   SolidColorBrush: {
@@ -132,7 +229,10 @@ let Schema = {
   GradientBrush: {
     type: "abstract",
     attrs: {
-      GradientStops: null
+      ColorInterpolationMode: ["ScRgbLinearInterpolation", "SRgbLinearInterpolation"],
+      GradientStops: null,
+      MappingMode: ["Absolute", "RelativeToBoundingBox"],
+      SpreadMethod: ["Pad", "Reflect", "Repeat"]
     },
     base: "Brush",
     children: ["GradientStop"]
@@ -225,6 +325,128 @@ let Schema = {
       CenterY: null
     },
     base: "Transform"
+  },
+  Geometry: {
+    attrs: {
+      Transform: "Transform"
+    }
+  },
+  GeometryGroup: {
+    attrs: {
+      Children: "Transform",
+      FillRule: ["EvenOdd", "Nonzero"]
+    },
+    base: "Geometry",
+    children: ["Geometry"]
+  },
+  CombinedGeometry: {
+    attrs: {
+      Geometry1: "Geometry",
+      Geometry2: "Geometry",
+      GeometryCombineMode: ["Union", "Intersect", "Xor", "Exclude"]
+    },
+    base: "Geometry"
+  },
+  RectangleGeometry: {
+    attrs: {
+      RadiusX: null,
+      RadiusY: null,
+      Rect: null
+    },
+    base: "Geometry"
+  },
+  EllipseGeometry: {
+    attrs: {
+      Center: null,
+      RadiusX: null,
+      RadiusY: null
+    },
+    base: "Geometry"
+  },
+  LineGeometry: {
+    attrs: {
+      StartPoint: null,
+      EndPoint: null
+    },
+    base: "Geometry"
+  },
+  StreamGeometry: {
+    attrs: {
+      FillRule: ["EvenOdd", "Nonzero"]
+    },
+    base: "Geometry"
+  },
+  PathGeometry: {
+    attrs: {
+      FillRule: ["EvenOdd", "Nonzero"],
+      Figures: "PathFigure"
+    },
+    base: "Geometry",
+    children: ["PathFigure"]
+  },
+  PathFigure: {
+    attrs: {
+      IsClosed: ["True", "False"],
+      IsFilled: ["True", "False"],
+      Segments: "PathSegment"
+    },
+    children: ["PathSegment"]
+  },
+  PathSegment: {
+    type: "abstract",
+    attrs: {
+      IsSmoothJoin: ["True", "False"],
+      IsStroked: ["True", "False"]
+    }
+  },
+  ArcSegment: {
+    attrs: {
+      Point: null,
+      Size: null,
+      RotationAngle: null,
+      IsLargeArc: ["True", "False"],
+      SweepDirection: ["Counterclockwise", "Clockwise"]
+    },
+    base: "PathSegment"
+  },
+  LineSegment: {
+    attrs: {
+      Point: null
+    },
+    base: "PathSegment"
+  },
+  QuadraticBezierSegment: {
+    attrs: {
+      Point1: null,
+      Point2: null
+    },
+    base: "PathSegment"
+  },
+  BezierSegment: {
+    attrs: {
+      Point1: null,
+      Point2: null,
+      Point3: null
+    },
+    base: "PathSegment"
+  },
+  PolyLineSegment: {
+    attrs: {
+      Points: null
+    },
+    base: "PathSegment"
+  },
+  PolyQuadraticBezierSegment: {
+    attrs: {
+      Points: null
+    },
+    base: "PathSegment"
+  },
+  PolyBezierSegment: {
+    attrs: {
+      Points: null
+    },
+    base: "PathSegment"
   },
   FrameworkTemplate: {
     type: "abstract",
